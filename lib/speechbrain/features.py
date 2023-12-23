de7a6f68785275cb549b59b4ff6d5ebea5a6a2d6
Amit Adhikari <amitadkri@gmail.com>
2023-12-23 12:42:17 -0800

    filterbank in mlx

--- lib/speechbrain/features.py
+++ lib/speechbrain/features.py
@@ -1,9 +1,10 @@
 """
-Features code from speechbrain
+Features code from speechbrain 
 (https://github.com/speechbrain/speechbrain/blob/develop/speechbrain/processing/features.py)
 """
 
 from typing import Any
+import math
 import mlx.nn as nn
 import mlx.core as mx
 import numpy as np
@@ -144,9 +145,362 @@ class STFT(nn.Module):
         return self.forward(*args, **kwds)
 
 
+class Filterbank(nn.Module):
+    """computes filter bank (FBANK) features given spectral magnitudes.
+
+    Arguments
+    ---------
+    n_mels : float
+        Number of Mel filters used to average the spectrogram.
+    log_mel : bool
+        If True, it computes the log of the FBANKs.
+    filter_shape : str
+        Shape of the filters ('triangular', 'rectangular', 'gaussian').
+    f_min : int
+        Lowest frequency for the Mel filters.
+    f_max : int
+        Highest frequency for the Mel filters.
+    n_fft : int
+        Number of fft points of the STFT. It defines the frequency resolution
+        (n_fft should be<= than win_len).
+    sample_rate : int
+        Sample rate of the input audio signal (e.g, 16000)
+    power_spectrogram : float
+        Exponent used for spectrogram computation.
+    amin : float
+        Minimum amplitude (used for numerical stability).
+    ref_value : float
+        Reference value used for the dB scale.
+    top_db : float
+        Minimum negative cut-off in decibels.
+    freeze : bool
+        If False, it the central frequency and the band of each filter are
+        added into nn.parameters. If True, the standard frozen features
+        are computed.
+    param_change_factor: bool
+        If freeze=False, this parameter affects the speed at which the filter
+        parameters (i.e., central_freqs and bands) can be changed.  When high
+        (e.g., param_change_factor=1) the filters change a lot during training.
+        When low (e.g. param_change_factor=0.1) the filter parameters are more
+        stable during training
+    param_rand_factor: float
+        This parameter can be used to randomly change the filter parameters
+        (i.e, central frequencies and bands) during training.  It is thus a
+        sort of regularization. param_rand_factor=0 does not affect, while
+        param_rand_factor=0.15 allows random variations within +-15% of the
+        standard values of the filter parameters (e.g., if the central freq
+        is 100 Hz, we can randomly change it from 85 Hz to 115 Hz).
+
+    Example
+    -------
+    >>> import torch
+    >>> compute_fbanks = Filterbank()
+    >>> inputs = torch.randn([10, 101, 201])
+    >>> features = compute_fbanks(inputs)
+    >>> features.shape
+    torch.Size([10, 101, 40])
+    """
+
+    def __init__(
+        self,
+        n_mels=40,
+        log_mel=True,
+        filter_shape="triangular",
+        f_min=0,
+        f_max=8000,
+        n_fft=400,
+        sample_rate=16000,
+        power_spectrogram=2,
+        amin=1e-10,
+        ref_value=1.0,
+        top_db=80.0,
+        param_change_factor=1.0,
+        param_rand_factor=0.0,
+        freeze=True,
+    ):
+        super().__init__()
+        self.n_mels = n_mels
+        self.log_mel = log_mel
+        self.filter_shape = filter_shape
+        self.f_min = f_min
+        self.f_max = f_max
+        self.n_fft = n_fft
+        self.sample_rate = sample_rate
+        self.power_spectrogram = power_spectrogram
+        self.amin = amin
+        self.ref_value = ref_value
+        self.top_db = top_db
+        self.freeze = freeze
+        self.n_stft = self.n_fft // 2 + 1
+        self.db_multiplier = math.log10(max(self.amin, self.ref_value))
+        self.param_change_factor = param_change_factor
+        self.param_rand_factor = param_rand_factor
+
+        if self.power_spectrogram == 2:
+            self.multiplier = 10
+        else:
+            self.multiplier = 20
+
+        # Make sure f_min < f_max
+        if self.f_min >= self.f_max:
+            err_msg = "Require f_min: %f < f_max: %f" % (
+                self.f_min,
+                self.f_max,
+            )
+            # logger.error(err_msg, exc_info=True)
+
+        # Filter definition
+        mel = mx.linspace(
+            self._to_mel(self.f_min), self._to_mel(self.f_max), self.n_mels + 2
+        )
+        hz = self._to_hz(mel)
+
+        # Computation of the filter bands
+        band = hz[1:] - hz[:-1]
+        self.band = band[:-1]
+        self.f_central = hz[1:-1]
+
+        # Adding the central frequency and the band to the list of nn param
+        if not self.freeze:
+            self.f_central = np.array(
+                self.f_central / (self.sample_rate * self.param_change_factor)
+            )
+            self.band = np.array(
+                self.band / (self.sample_rate * self.param_change_factor)
+            )
+
+        # Frequency axis
+        all_freqs = mx.linspace(0, self.sample_rate // 2, self.n_stft)
+
+        # Replicating for all the filters
+        all_freqs_mat = np.tile(all_freqs, [self.f_central.shape[0], 1])
+        self.all_freqs_mat = mx.array(all_freqs_mat)
+        print("all_freqs_mat.shape", self.all_freqs_mat.shape)
+        # self.all_freqs_mat = all_freqs.repeat(self.f_central.shape[0], 1)
+
+    def forward(self, spectrogram):
+        """Returns the FBANks.
+
+        Arguments
+        ---------
+        x : tensor
+            A batch of spectrogram tensors.
+        """
+        # Computing central frequency and bandwidth of each filter
+        f_central_mat = mx.array(
+            np.tile(self.f_central, [self.all_freqs_mat.shape[1], 1]).transpose(1, 0)
+        )
+        band_mat = mx.array(
+            np.tile(self.band, [self.all_freqs_mat.shape[1], 1]).transpose(1, 0)
+        )
+
+        # Uncomment to print filter parameters
+        # print(self.f_central*self.sample_rate * self.param_change_factor)
+        # print(self.band*self.sample_rate* self.param_change_factor)
+
+        # Creation of the multiplication matrix. It is used to create
+        # the filters that average the computed spectrogram.
+        if not self.freeze:
+            f_central_mat = f_central_mat * (
+                self.sample_rate * self.param_change_factor * self.param_change_factor
+            )
+            band_mat = band_mat * (
+                self.sample_rate * self.param_change_factor * self.param_change_factor
+            )
+
+        fbank_matrix = self._create_fbank_matrix(f_central_mat, band_mat)
+        sp_shape = spectrogram.shape
+
+        # Managing multi-channels case (batch, time, channels)
+        if len(sp_shape) == 4:
+            spectrogram = spectrogram.permute(0, 3, 1, 2)
+            spectrogram = spectrogram.reshape(
+                sp_shape[0] * sp_shape[3], sp_shape[1], sp_shape[2]
+            )
+
+        # FBANK computation
+        fbanks = mx.matmul(spectrogram, fbank_matrix)
+        if self.log_mel:
+            fbanks = self._amplitude_to_DB(fbanks)
+
+        # Reshaping in the case of multi-channel inputs
+        if len(sp_shape) == 4:
+            fb_shape = fbanks.shape
+            fbanks = fbanks.reshape(sp_shape[0], sp_shape[3], fb_shape[1], fb_shape[2])
+            fbanks = fbanks.permute(0, 2, 3, 1)
+
+        return fbanks
+
+    @staticmethod
+    def _to_mel(hz):
+        """Returns mel-frequency value corresponding to the input
+        frequency value in Hz.
+
+        Arguments
+        ---------
+        x : float
+            The frequency point in Hz.
+        """
+        return 2595 * math.log10(1 + hz / 700)
+
+    @staticmethod
+    def _to_hz(mel):
+        """Returns hz-frequency value corresponding to the input
+        mel-frequency value.
+
+        Arguments
+        ---------
+        x : float
+            The frequency point in the mel-scale.
+        """
+        return mx.array(700 * (10 ** (np.array(mel) / 2595) - 1))
+
+    def _triangular_filters(self, all_freqs, f_central, band):
+        """Returns fbank matrix using triangular filters.
+
+        Arguments
+        ---------
+        all_freqs : Tensor
+            Tensor gathering all the frequency points.
+        f_central : Tensor
+            Tensor gathering central frequencies of each filter.
+        band : Tensor
+            Tensor gathering the bands of each filter.
+        """
+
+        # Computing the slops of the filters
+        slope = (all_freqs - f_central) / band
+        left_side = slope + 1.0
+        right_side = -slope + 1.0
+
+        # Adding zeros for negative values
+        zero = mx.zeros(1)
+        fbank_matrix = np.minimum(zero, np.minimum(left_side, right_side)).transpose(
+            1, 0
+        )
+
+        return mx.array(fbank_matrix)
+
+    def _rectangular_filters(self, all_freqs, f_central, band):
+        """Returns fbank matrix using rectangular filters.
+
+        Arguments
+        ---------
+        all_freqs : Tensor
+            Tensor gathering all the frequency points.
+        f_central : Tensor
+            Tensor gathering central frequencies of each filter.
+        band : Tensor
+            Tensor gathering the bands of each filter.
+        """
+
+        # cut-off frequencies of the filters
+        low_hz = f_central - band
+        high_hz = f_central + band
+
+        # Left/right parts of the filter
+        left_side = right_size = all_freqs.ge(low_hz)
+        right_size = all_freqs.le(high_hz)
+
+        fbank_matrix = (left_side * right_size).float().transpose(0, 1)
+
+        return fbank_matrix
+
+    def _gaussian_filters(
+        self, all_freqs, f_central, band, smooth_factor=torch.tensor(2)
+    ):
+        """Returns fbank matrix using gaussian filters.
+
+        Arguments
+        ---------
+        all_freqs : Tensor
+            Tensor gathering all the frequency points.
+        f_central : Tensor
+            Tensor gathering central frequencies of each filter.
+        band : Tensor
+            Tensor gathering the bands of each filter.
+        smooth_factor: Tensor
+            Smoothing factor of the gaussian filter. It can be used to employ
+            sharper or flatter filters.
+        """
+        fbank_matrix = torch.exp(
+            -0.5 * ((all_freqs - f_central) / (band / smooth_factor)) ** 2
+        ).transpose(0, 1)
+
+        return fbank_matrix
+
+    def _create_fbank_matrix(self, f_central_mat, band_mat):
+        """Returns fbank matrix to use for averaging the spectrum with
+           the set of filter-banks.
+
+        Arguments
+        ---------
+        f_central : Tensor
+            Tensor gathering central frequencies of each filter.
+        band : Tensor
+            Tensor gathering the bands of each filter.
+        smooth_factor: Tensor
+            Smoothing factor of the gaussian filter. It can be used to employ
+            sharper or flatter filters.
+        """
+        if self.filter_shape == "triangular":
+            fbank_matrix = self._triangular_filters(
+                self.all_freqs_mat, f_central_mat, band_mat
+            )
+
+        elif self.filter_shape == "rectangular":
+            fbank_matrix = self._rectangular_filters(
+                self.all_freqs_mat, f_central_mat, band_mat
+            )
+
+        else:
+            fbank_matrix = self._gaussian_filters(
+                self.all_freqs_mat, f_central_mat, band_mat
+            )
+
+        return fbank_matrix
+
+    def _amplitude_to_DB(self, x):
+        """Converts  linear-FBANKs to log-FBANKs.
+
+        Arguments
+        ---------
+        x : Tensor
+            A batch of linear FBANK tensors.
+
+        """
+
+        print(type(x))
+
+        x_db = self.multiplier * mx.log10(
+            mx.array(np.clip(a=x, a_min=self.amin, a_max=None))
+        )
+        x_db -= self.multiplier * self.db_multiplier
+
+        # Setting up dB max. It is the max over time and frequency,
+        # Hence, of a whole sequence (sequence-dependent)
+        new_x_db_max = x_db.max(axis=(-2, -1)) - self.top_db
+
+        # Clipping to dB max. The view is necessary as only a scalar is obtained
+        # per sequence.
+        # x_db = torch.max(x_db, new_x_db_max.view(x_db.shape[0], 1, 1))
+        x_db = np.maximum(x_db, new_x_db_max.reshape(x_db.shape[0], 1, 1))
+
+        return x_db
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
+
+
 if __name__ == "__main__":
     # test STFT
     x = mx.random.normal([1, 4001])
     compute_STFT = STFT(sample_rate=16000, win_length=25, hop_length=10, n_fft=400)
     features = compute_STFT(x)
+    print(features.shape)
+
+    # test Filterbank
+    compute_fbanks = Filterbank()
+    inputs = mx.random.normal([10, 101, 201])
+    features = compute_fbanks(inputs)
     print(features.shape)
